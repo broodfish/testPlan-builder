d8a4c799e3403599082f6ced42b249a75eb2d9d8
broodfish <sag860610@gmail.com>
2024-09-04 15:53:45 +0800

    refactor: Add Suite component for displaying test cases in a plan

---
+++ components/Plan/Suite.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="border border-primary rounded-lg overflow-hidden">
+    <v-hover>
+      <template #default="{ isHovering, props }">
+        <div
+          v-ripple="{ class: 'text-primary-lighten-1' }"
+          v-bind="props"
+          class="px-4 py-3 cursor-pointer tw-flex tw-flex-row tw-items-center tw-justify-between tw-gap-4"
+          :class="isHovering ? 'background-hover' : 'background'"
+          @click="isExpanded = !isExpanded"
+        >
+          <div>
+            <v-icon
+              class="tw-transition"
+              color="primary-darken-1"
+              :class="isExpanded ? 'tw-rotate-90' : ''"
+              >arrow_right</v-icon
+            >
+            <span class="text-primary ml-2 tw-font-bold">
+              {{ propsData.title }}
+            </span>
+          </div>
+          <div @click.stop>
+            <v-btn prepend-icon="add" @click="adding = true">New Case</v-btn>
+          </div>
+        </div>
+      </template>
+    </v-hover>
+    <v-expand-transition>
+      <div v-show="isExpanded">
+        <v-table class="border-none px-4 py-3">
+          <thead>
+            <tr>
+              <th class="tw-flex-1">Case Name</th>
+              <th class="text-center tw-w-[80px]">Steps</th>
+              <th class="text-center tw-w-[120px]">Priority</th>
+              <th class="tw-w-[44px]"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <v-hover v-for="item in propsData.items" :key="item.id">
+              <template #default="{ isHovering, props }">
+                <tr
+                  v-ripple="{ class: 'text-ripple' }"
+                  v-bind="props"
+                  class="cursor-pointer"
+                  :class="isHovering ? 'bg-shadow' : 'bg-transparent'"
+                  @click="
+                    navigateTo(
+                      `/${route.params.proID}/${route.params.planID}/${item.gid}/${item.id}`,
+                    )
+                  "
+                >
+                  <td>{{ item.description }}</td>
+                  <td class="text-center">{{ item.steps.num }}</td>
+                  <td class="tw-text-center">
+                    <v-chip v-if="item.priority === 'High'" color="error"
+                      >High</v-chip
+                    >
+                    <v-chip
+                      v-else-if="item.priority === 'Medium'"
+                      color="primary"
+                      >Medium</v-chip
+                    >
+                    <v-chip v-else>Low</v-chip>
+                  </td>
+                  <td>
+                    <menu-actions
+                      @edit="(currentCase = item), (editing = true)"
+                      @delete="deleting = true"
+                    ></menu-actions>
+                  </td>
+                </tr>
+              </template>
+            </v-hover>
+          </tbody>
+        </v-table>
+      </div>
+    </v-expand-transition>
+    <dialog-new-case v-model="adding"></dialog-new-case>
+    <dialog-edit-case v-model="editing" :data="currentCase"></dialog-edit-case>
+    <dialog-confirm-deletion
+      v-model="deleting"
+      @delete="console.log('delete')"
+    ></dialog-confirm-deletion>
+  </div>
+</template>
+<script setup lang="ts">
+const route = useRoute();
+const propsData = defineProps<{
+  title: string;
+  items: Case[];
+}>();
+const isExpanded = ref(true);
+const adding = ref(false);
+const editing = ref(false);
+const deleting = ref(false);
+
+const currentCase = ref<Case>({
+  id: -1,
+  gid: -1,
+  description: "",
+  priority: "Medium",
+  prerequisite: "",
+  steps: {
+    num: 0,
+    data: [],
+  },
+});
+</script>
+<style scoped lang="scss">
+.background {
+  background-color: rgba(var(--v-theme-primary), 0.1);
+
+  &-hover {
+    background-color: rgba(var(--v-theme-primary), 0.15);
+  }
+}
+</style>
--- pages/[proID]/[planID]/index.vue
+++ pages/[proID]/[planID]/index.vue
@@ -1,131 +1,26 @@
 <template>
   <div>
-    <layouts-default-title class="tw-justify-between">
-      <div class="tw-flex tw-gap-4">
-        Test Cases
-        <v-btn-group color="tertiaryContainer"
-          ><v-btn @click="adding = true">New</v-btn
-          ><v-btn icon size="small"
-            ><v-icon icon="arrow_drop_down"></v-icon>
-            <v-menu activator="parent">
-              <v-list>
-                <div class="tw-flex tw-flex-col">
-                  <v-btn
-                    class="text-btn"
-                    variant="text"
-                    :rounded="false"
-                    @click="adding = true"
-                  >
-                    New Test Case
-                  </v-btn>
-                  <v-btn class="text-btn" variant="text" :rounded="false"
-                    >New Test Suite
-                    <menu-new-suite></menu-new-suite>
-                  </v-btn>
-                </div>
-              </v-list>
-            </v-menu> </v-btn
-        ></v-btn-group>
-      </div>
-      <v-btn
-        prepend-icon="play_arrow"
-        color="primary"
-        @click="
-          () => {
-            newRunData = getPlan(
-              Number(route.params.proID),
-              Number(route.params.planID),
-            )!;
-            creating = true;
-          }
-        "
-      >
-        Run Test Plan</v-btn
-      >
-    </layouts-default-title>
-    <div v-if="testCases?.length">
-      <div class="tw-flex tw-flex-col tw-gap-14">
-        <div v-for="(group, i) in testCases" :key="i">
-          <span class="subtitle">
-            {{ group.name.toUpperCase() }}
-          </span>
-          <v-data-table-virtual
+    <custom-card title="Test Cases">
+      <template #content>
+        <div class="tw-flex tw-flex-col tw-gap-4">
+          <plan-suite
+            v-for="(group, i) in testCases"
+            :key="i"
+            :title="group.name"
             :items="group.cases.data"
-            :headers="headers"
-            no-data-text="No Test Case"
           >
-            <template #item="{ item }">
-              <tr>
-                <td>{{ item.id }}</td>
-                <td
-                  v-ripple
-                  class="hover:tw-cursor-pointer hover:tw-bg-[rgba(0,0,0,0.04)]"
-                  @click="
-                    navigateTo(
-                      `/${route.params.proID}/testPlans/${route.params.planID}/${group.id}/${item.id}`,
-                    )
-                  "
-                >
-                  {{ item.description }}
-                </td>
-                <td>{{ item.steps.num }}</td>
-                <td class="tw-text-center">
-                  <v-chip v-if="item.priority === 'High'" color="error"
-                    >High</v-chip
-                  >
-                  <v-chip v-else-if="item.priority === 'Medium'" color="primary"
-                    >Medium</v-chip
-                  >
-                  <v-chip v-else>Low</v-chip>
-                </td>
-                <td>
-                  <menu-actions
-                    @edit="
-                      () => {
-                        currentCase = item;
-                        console.log(currentCase);
-                        editing = true;
-                      }
-                    "
-                    @delete="deleting = true"
-                  ></menu-actions>
-                </td>
-              </tr>
-            </template>
-          </v-data-table-virtual>
-          <v-btn
-            prepend-icon="add"
-            variant="plain"
-            class="tw-ml-4 tw-mt-2"
-            color="primary"
-            >New Test Case <menu-new-case></menu-new-case>
-          </v-btn>
+          </plan-suite>
         </div>
-      </div>
-      <div class="tw-mt-8">
-        <v-btn prepend-icon="add" color="tertiaryContainer"
-          >New Test Suite
-          <menu-new-suite></menu-new-suite>
-        </v-btn>
-      </div>
-    </div>
-    <v-empty-state
-      v-else
-      headline="(´･_･`)"
-      title="No Test Case Yet"
-      text="Click 'NEW' button to add a test case!"
-    ></v-empty-state>
+      </template>
+      <template #action>
+        <v-btn prepend-icon="add">New Suite</v-btn>
+      </template>
+    </custom-card>
     <dialog-create-run
       v-model="creating"
       :item="newRunData"
       @create="console.log('create')"
     ></dialog-create-run>
-    <dialog-new-case v-model="adding"></dialog-new-case>
-    <dialog-edit-case v-model="editing" :data="currentCase"></dialog-edit-case>
-    <dialog-confirm-deletion
-      v-model="deleting"
-      @delete="console.log('delete')"
-    ></dialog-confirm-deletion>
   </div>
 </template>
 <script setup lang="ts">
@@ -136,9 +31,6 @@ definePageMeta({
 const route = useRoute();
 
 const creating = ref(false);
-const adding = ref(false);
-const editing = ref(false);
-const deleting = ref(false);
 const newRunData = ref<Plan>({
   id: -1,
   title: "",
@@ -149,44 +41,7 @@ const newRunData = ref<Plan>({
     data: [],
   },
 });
-const currentCase = ref<Case>({
-  id: -1,
-  gid: -1,
-  description: "",
-  priority: "Medium",
-  prerequisite: "",
-  steps: {
-    num: 0,
-    data: [],
-  },
-});
 
-const headers: ReadonlyHeaders = [
-  {
-    title: "ID",
-    value: "id",
-    width: "60",
-  },
-  {
-    title: "Description",
-    value: "description",
-  },
-  {
-    title: "Steps",
-    align: "center",
-    width: "60",
-  },
-  {
-    title: "Priority",
-    align: "center",
-    width: "100",
-  },
-  {
-    key: "options",
-    width: "44",
-    sortable: false,
-  },
-];
 const testCases = computed(() =>
   getCases(Number(route.params.proID), Number(route.params.planID)),
 );
